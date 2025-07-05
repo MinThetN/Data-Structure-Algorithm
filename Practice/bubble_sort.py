a01ed395ccd6259fa75801f83702a874cc3f6368
MinThetN <minthetn15@gmail.com>
2025-07-05 16:53:30 +0700

    modified

--- Practice/bubble_sort.py
+++ Practice/bubble_sort.py
@@ -2,16 +2,17 @@
 # Time : 0(n^2)
 # Space : 0(1)
 
-A = [-5, 1, -6, 7, 3, 5, 2, 4, 1]
+A = [9, -5, -7, 3, 6, 2, 1]
 def bubble_sort(arr):
     n = len(arr)
     run = True
+
     while run:
         run = False
         for i in range(1, n):
             if arr[i - 1] > arr[i]:
                 run = True
-                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                arr[i - 1], arr[i] = arr[i], arr[i -1]
 
 bubble_sort(A)
-print(A)
+print("sorted array", A)
